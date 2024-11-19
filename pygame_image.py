10632d98524125daa9f3444bf960300452f6c062
C0B23003 <c0b23003ec@edu.teu.ac.jp>
2024-11-19 16:15:01 +0900

    間延び解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")#surfaceクラスのインスタンス
+    bg_img_flipped = pg.transform.flip(bg_img, True, False)
     koukaton_image = pg.image.load("fig/3.png")
     koukaton_image = pg.transform.flip(koukaton_image, True, False)
     tmr = 0
@@ -17,12 +18,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(koukaton_image, [300, 200])
+        x = -(tmr%3200)
+        screen.blit(bg_img,  [x, 0])
+        screen.blit(bg_img_flipped, [x+1600, 0])
+        screen.blit(bg_img,  [x+3200, 0])
+        screen.blit(bg_img_flipped, [x+4800, 0])
+        screen.blit(koukaton_image, [300, 200])  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+        
         
 
 
