03474111b665cb8550c9bae30b0a20bf69bd2ed1
C0B23003 <c0b23003ec@edu.teu.ac.jp>
2024-11-19 15:55:45 +0900

    aa

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")#surfaceクラスのインスタンス
-    bgg_img = pg.image.load("3.png")
+    koukaton_image = pg.image.load("fig/3.png")
+    koukaton_image = pg.transform.flip(koukaton_image, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bgg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(koukaton_image, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
+        
 
 
 if __name__ == "__main__":
